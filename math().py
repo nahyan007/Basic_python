ef7220424b55df83b4b5d8836d2db1a2ab28e610
nahyan007 <shorifrex@gmail.com>
2024-06-16 23:22:12 +0600

    simple math functions

---
+++ math().py
@@ -0,0 +1,14 @@
+import math
+
+pi = 3.14159265359
+x = 2
+y = 4
+z = 8
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(math.sqrt(pi))
+print(max(pi,x,y,z))
+print(min(pi,x,y,z))
