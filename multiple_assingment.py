c9bb83bc82386c8cf4d160c58e3a986d30550744
nahyan007 <shorifrex@gmail.com>
2024-06-16 20:43:48 +0600

    multiple_assingment

---
+++ multiple_assingment.py
@@ -0,0 +1,9 @@
+# multiple assignment: allow us to assign multiple variables at the same time in one line of code
+
+car_model, car_year, car_is_attractive = "Supra", 2013 , True
+print(car_model, car_year , car_is_attractive)
+
+
+toyota_gt = mustang = supra = 135
+print('toyota gt speed:' + str(toyota_gt) + 'kmh', 'mustang speed:' + str(mustang) + 'kmh' , 'supra speed:' + str(supra) + 'kmh')
+print('speed:' + str(toyota_gt),str(mustang),str(supra)  + 'kmh')
