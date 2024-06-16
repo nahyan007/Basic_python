69dac69e955441dd0954e42350f76f440be5fca2
nahyan007 <shorifrex@gmail.com>
2024-06-16 18:16:21 +0600

    basic variables

---
+++ variable.py
@@ -0,0 +1,26 @@
+# # 4types of variable 
+
+# stirng type which refer to us as str
+Name= "Mazda"
+print(Name)
+print(type(Name))
+
+
+# integer type which refer to us as int
+model = 2013
+print(model)
+print(type(model))
+
+
+# float type which refer to us as float
+Milage = 17.6
+print(Milage)
+print(type(Milage))
+
+
+# boolean type which refer to us as bool
+isCar = True
+print(isCar)
+
+car = Name + " " + str(model) + " " + str(Milage) + " " +  str(isCar)
+print(car)
