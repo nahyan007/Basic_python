e51c0ebc2762c42868376338cf248a00898435ba
nahyan007 <shorifrex@gmail.com>
2024-06-16 21:00:39 +0600

    usefull_methods

---
+++ python_method.py
@@ -0,0 +1,12 @@
+Car = 'Mustang'
+
+print(len(Car))
+print(Car.find('a'))
+print(Car.capitalize())
+print(Car.upper())
+print(Car.lower())
+print(Car.isdigit())
+print(Car.isalpha())
+print(Car.count('t'))
+print(Car.replace('u', 'a'))
+print(Car*10)
