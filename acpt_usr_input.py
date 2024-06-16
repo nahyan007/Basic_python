c4c5a11f844961719f0597d0a507e4530b4feffc
nahyan007 <shorifrex@gmail.com>
2024-06-16 22:28:53 +0600

    accept user input

---
+++ acpt_usr_input.py
@@ -0,0 +1,10 @@
+car = input("Whats your car name? ")
+model = int(input("Whats your car model? "))
+height = float(input("Whats your car Height? "))
+
+model += 1
+height -= 2.90
+
+print("Your car name is "+ car)
+print("Your car model is "+ str(model) + " year edition")
+print("Your car height is "+ str(height) + " cm tall")
