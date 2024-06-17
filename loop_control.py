7743fc6540d2140ad5ca131fcf7cc0bc6470e19c
nahyan007 <shorifrex@gmail.com>
2024-06-18 03:16:20 +0600

    loop control template

---
+++ loop_control.py
@@ -0,0 +1,7 @@
+# loop control statement = change a loops execution from its normal sequence 
+
+# break = used to terminate the loop entirely
+# continue = skips the current iteration and moves on to the next one
+# pass = does nothing, acts as a placeholder
+
+
