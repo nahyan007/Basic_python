051ef4a257310719ffb5744203a19e8db497491c
nahyan007 <shorifrex@gmail.com>
2024-06-18 01:37:20 +0600

    while loop

---
+++ while_loop.py
@@ -0,0 +1,14 @@
+# while loop = a statement that will execute its block of code, as long as its conditional remains true
+
+# name =""
+# short hand
+name = None
+
+# while len(name) == 0:
+# short hand
+while not name:
+    name = input("Enter your name: ")
+    
+    
+print("Hello " + name)
+    
