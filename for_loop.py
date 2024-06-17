b27be6b4f70cccc71dede287a8c6e65523082c6e
nahyan007 <shorifrex@gmail.com>
2024-06-18 01:58:18 +0600

    for loop and time module

---
+++ for_loop.py
@@ -0,0 +1,23 @@
+
+# while loop = unlimited
+# for loop = limited
+
+
+# for i in range(10):
+#     print(i+1)
+    
+
+# for i in range(50, 100+1,2):  #(start,end,step)
+#       print(i)
+
+# for name in "Nahian Avid":
+#     print(name)  # it will print each character of the string
+    
+
+import time
+
+ 
+for sec in range (10,0,-1): #(start,end,step)
+    print(sec)
+    time.sleep(1) #import time module
+print("Happy New Year!")  # countdown from 10 to 1 and then print "Happy
