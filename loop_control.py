6ee068df183645cc94c516d961b664781445ccc5
nahyan007 <shorifrex@gmail.com>
2024-06-19 12:58:01 +0600

    loop control

--- loop_control.py
+++ loop_control.py
@@ -5,3 +5,22 @@
 # pass = does nothing, acts as a placeholder
 
 
+# while True:
+#     name = input("Please enter your name: ")
+#     if name != "":
+#         break
+
+
+# num = input("Please enter number & -: ")
+
+# for i in num:
+#     if i == "-":
+#         continue
+#     print(i, end="")
+
+
+for i in range(1,20):
+    if i == 13:
+        pass
+    else:
+        print(i)
