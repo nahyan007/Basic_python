fdf7fcfdf6e193f0ab0ebd720e5220a98c05daf3
nahyan007 <shorifrex@gmail.com>
2024-06-19 13:30:35 +0600

    2d list

---
+++ 2d_list.py
@@ -0,0 +1,13 @@
+# 2d lists = a list of lists
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert= ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+
+# for i in food:
+#     print(i)
+
+print(i)
