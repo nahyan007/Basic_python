975b773927d309b19533aabba572a34ee753fbf2
nahyan007 <shorifrex@gmail.com>
2024-06-19 13:42:13 +0600

    tuple

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# tuple = collection which is ordered and unchangeable used to group together related data
+
+# list []  tuple ()
+
+
+student = ("bro",21,"male")
+
+print(student.count("bro"))
+print(student.index(21))
+
+for i in student:
+    print(i, end=" ")
+    
