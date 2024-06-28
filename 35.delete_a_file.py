77685ab85c93a02556250ac616a1d8ce1245c160
nahyan007 <shorifrex@gmail.com>
2024-06-29 04:09:21 +0600

    delete a folder

--- 35.delete_a_file.py
+++ 35.delete_a_file.py
@@ -1,9 +1,23 @@
 import os
 
-path = "35.delete_a_file.txt"
+# path = "35.delete_a_file.txt"
+
+# try:
+#     os.remove(path)
+#     print(path + " was deleted")
+# except FileNotFoundError:
+#     print("that file does not exist")
+    
+
+# delete a folder
+
+path = "empty_folder"
 
 try:
-    os.remove(path)
-    print(path + " was deleted")
+    os.rmdir(path)
 except FileNotFoundError:
-    print("that file does not exist")
+    print("that folder does not exist")
+except PermissionError:
+    print("not allowed to delete")
+else:
+    print(path + " was deleted")
