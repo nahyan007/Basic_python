7573572c70c0771355006a9191c11ddd0f9012b0
nahyan007 <shorifrex@gmail.com>
2024-06-29 04:30:09 +0600

    modules

---
+++ 36.modules.py
@@ -0,0 +1,9 @@
+# module = a file containing python code,function,classes etc.
+# used with modular programming which is to separate a program into parts. 
+
+# import module_name
+# import module_name as module_nickname
+# from module_name hello,bye    #for specific things from a module
+# from module_name *            #for all things from a module
+
+help("modules")
